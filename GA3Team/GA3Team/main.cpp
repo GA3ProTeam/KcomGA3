a7841af39b2d72a09477a949472b9a5cb730e7e9
Fujimoto <h1201515024@hiratagakuen.onmicrosoft.com>
2017-04-25 10:25:02 +0900

    ライブラリ参照セット

--- GA3Team/GA3Team/main.cpp
+++ GA3Team/GA3Team/main.cpp
@@ -1,11 +1,83 @@
-#include <stdio.h>
-#include <Windows.h>
-#include <conio.h>
+#include "main.h"
 
-void main() {
-	int i;
-	int j;
-	int x;
-	int y;
-	return;
+
+
+
+//プロトタイプ宣言
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+//Main関数
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR szCmdLine, int nCmdShow)
+{
+	char Name[] = { "ゲームエンジン01" };
+	//ウィンドウステータス
+	WNDCLASSEX wcex = {
+		sizeof(WNDCLASSEX),
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0, 0,
+		hInstance,
+		NULL, NULL,
+		(HBRUSH)(COLOR_WINDOW + 1),
+		NULL,Name,NULL
+	};
+
+	//ウィンドウクラス作成
+	if (!RegisterClassEx(&wcex))
+		return false;
+
+	//ウィンドウ作成
+	if (!(hWnd = CreateWindow(Name, Name, WS_OVERLAPPEDWINDOW & ~(WS_MAXIMIZEBOX | WS_SIZEBOX),
+		CW_USEDEFAULT, 0, WINDOW_SIZE_W, WINDOW_SIZE_H, 0, 0, hInstance, 0)))
+		return false;
+	ShowWindow(hWnd, SW_SHOW);
+
+	//ゲームエンジン関連の初期化---------------------
+	InitLibrary();
+	srand((unsigned int)time(NULL));
+	//------------------------------------------------
+
+	//------始めのシーンを登録------------------------
+	g_SceneManager->Pop(NULL);
+	//------------------------------------------------
+
+	//------ゲームループ実行--------------------------
+	do
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		g_SceneManager->GameLoop();
+	} while (msg.message != WM_QUIT);
+	//------------------------------------------------
+
+	//-----ゲームエンジンの破棄-----------------------
+	EndLibrary();
+	//------------------------------------------------
+	return true;
+}
+
+//コールバック関数
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (g_WinInputs->WmInput(&uMsg, &lParam) == true) return 0;
+	switch (uMsg)
+	{
+	case WM_CLOSE:          // ウィンドウを閉じる場合
+		DestroyWindow(hWnd);	// ウィンドウを破棄し登録を解除します。
+		break;
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+		{
+			PostQuitMessage(0);
+			return 0;
+		}
+		break;
+	case WM_DESTROY:        // 終了する場合
+		PostQuitMessage(0);		 // アプリケーションを終了コード 0 で終了します。
+		return 0;
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
---
+++ GA3Team/GA3Team/main.h
@@ -0,0 +1,231 @@
+#ifndef MAIN_H__
+#define MAIN_H__
+
+//STLデバッグ機能をOFFにする
+#define _SECURE_SCL	(0)  
+#define _HAS_ITERATOR_DEBUGGING	(0)  
+
+//ヘッダー
+#include <stdio.h>
+#include <Windows.h>
+#include <D3D11.h> 
+#include <d3dx11.h>
+#include <d3dCompiler.h>
+#include <xnamath.h>
+#include <crtdbg.h>
+#include <XAudio2.h>
+#include <time.h>
+#include <stdlib.h>
+
+//STLヘッダ
+#include <iostream>
+#include <fstream>
+#include <vector>		
+#include <list>
+#include <set>
+#include <map>
+#include <algorithm>
+
+//DIB
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "d3d11.lib") 
+#pragma comment(lib, "d3dx11.lib")
+#pragma comment(lib, "d3dCompiler.lib")
+
+using namespace std;
+
+//開放マクロ
+#define SAFE_DELETE(p)       { if (p) { delete (p);     (p)=NULL; } }
+#define SAFE_DELETE_ARRAY(p) { if (p) { delete[] (p);   (p)=NULL; } }
+#define SAFE_RELEASE(p)      { if (p) { (p)->Release(); (p)=NULL; } }
+
+//列挙型
+//テクスチャイメージサイズ　８乗ｵﾝﾘ-設定
+enum TEX_SIZE
+{
+	TEX_SIZE_8	  =	8,
+	TEX_SIZE_16   =	16,
+	TEX_SIZE_24   =	24,
+	TEX_SIZE_32   =	32,
+	TEX_SIZE_64   =	64,
+	TEX_SIZE_128  =	128,
+	TEX_SIZE_256  =	256,
+	TEX_SIZE_512  =	512,
+	TEX_SIZE_1024 =	1024,
+};
+//オブジェクト状態
+enum OBJ_STATUS
+{
+	STATUS_EXE,		//実行
+	STATUS_SLEEP,	//停止
+	STATUS_DELETE,	//削除
+};
+//オブジェクトの当たり判定の有無
+enum OBJ_SET_HITBOX
+{
+	HIT_BOX_ON,
+	HIT_BOX_OFF,
+};
+//当たり判定属性---------
+enum HIT_ELEMENTS
+{
+	ELEMENT_NULL,//属性無し 全ての属性と当たり判定が実行される
+	//以下　同じ属性同士では当たり判定は実行されない
+	//属性は追加可能だが、デバック時の色は初期設定分しか無い
+	ELEMENT_PLAYER,
+	ELEMENT_ENEMY,
+	ELEMENT_ITEM,
+	ELEMENT_MAGIC,
+	ELEMENT_FIELD,
+	ELEMENT_LED,
+	ELEMENT_GREEN,
+	ELEMENT_BLUE,
+	ELEMENT_BLACK,
+	ELEMENT_WHITE,
+};
+//-----------------------
+//オブジェクトネーム-----
+enum OBJ_NAME
+{
+	OBJ_NO_NAME,
+	//ゲームで使うオブジェクトの名前
+	//OBJ_○○と表記
+	OBJ_PLAYER,
+	OBJ_BLOCKMANAGER,
+	OBJ_BACK,
+	OBJ_BALL,
+	OBJ_BLOCK,
+	OBJ_TITLE,
+	OBJ_RESULT,
+	OBJ_ITEM,
+};
+//----------------------
+
+//表示ウィンドウの大きさ
+#define WINDOW_SIZE_W	800	
+#define WINDOW_SIZE_H	600
+
+//解像度の大きさ
+#define PIXEL_SIZE_W	800	
+#define PIXEL_SIZE_H	600
+
+//一つのシーンで読み込めるイメージ総数
+#define SCENE_IMG_MAX	64
+//一列の文字列最大出力
+#define STR_LEN_MAX		128
+//一つのシーンで読み込める音楽総数
+#define SCENE_AUDIO_MAX	32
+//効果音の同時出力最大数
+#define SCENE_AUDIO_EFFCT_MAX	8
+
+//フレームレート
+#define DRAW_FPS	false	//FPSの描画
+
+//CUserDataでセーブ・オープンする名前
+#define SEVE_NAME "user_data.txt"
+
+//当たり判定表示(でバック用)
+#define HIT_DRAW	false
+
+//当たり判定の複数同時判定量
+#define MAX_HITS	15
+
+
+//------------------------------
+//バーの幅
+#define BAR_SIZE_W		72
+//バーの高さ
+#define BAR_SIZE_H		8
+//ブロックの幅
+#define BLOCK_SIZE_W	48
+//ブロックの高さ
+#define BLOCK_SIZE_H	24
+//ボールの幅
+#define BALL_SIZE_W		16
+//ボールの高さ
+#define BALL_SIZE_H		16
+//ブロックのxの量
+#define BLOCK_ARRAY_X	14
+//ブロックのyの量
+#define BLOCK_ARRAY_Y	5
+//ボールの最大量
+#define BALL_MAX		16
+//アイテムの幅
+#define ITEM_SIZE_W		32
+//アイテムの高さ
+#define ITEM_SIZE_H		32
+
+//------------------------------
+
+//ゲームエンジンクラス
+class CObj;
+class CScene;
+class CDirectXDeviec;
+class CDrawTexture;
+class CWinInputs;
+class CAudio;
+class CDrawFont;
+class CFontCharTex;
+class CSceneManager;
+class CSceneObjManager;
+class CUserData;
+class CHitBox;
+class CHitBoxManager;
+
+//ゲーム各エンジンクラス
+extern CDirectXDeviec*	g_DirectXDeveice;
+extern CDrawTexture*	g_DrawTexture;
+extern CWinInputs*		g_WinInputs;
+extern CAudio*			g_Audio;
+extern CDrawFont*		g_DrawFont;
+extern CSceneManager*   g_SceneManager;
+extern CSceneObjManager*g_SceneObjManager;
+extern CUserData*		g_UserData;
+extern CHitBoxManager*	g_HitBoxManager;
+
+//エンジンヘッダー
+#include "Library\\DirectXDevice.h"
+#include "Library\\DrawTexture.h"
+#include "Library\\WinInputs.h"
+#include "Library\\Audio.h"
+#include "Library\\DrawFont.h"
+#include "Library\\SceneManager.h"
+#include "Library\\SceneObjManager.h"
+#include "Library\\HitBoxManager.h"
+#include "Library\Library.h"
+#include "Library\Trace.h"
+
+
+
+//ゲームシーン・オブジェクトのインクルード-------
+//シーン１
+
+
+//----------------------------------------------
+
+
+
+//ユーザー・セーブ・シーン間受け渡しデータ
+//固定メモリのみ
+class CUserData
+{
+	public :
+		CUserData();
+		~CUserData();
+		void Open();
+		void Seve();
+		
+		//----ユーザーが持つデータ-------
+		int mSeveData;//サンプルセーブデータ
+
+		//-------------------------------
+
+		//ユーザーデータ以外の外部データ読み込み
+		//全データ(文字列情報)を取得したメモリが返される
+		char* ExternalDataOpen(char* file_name,int* size);
+};
+
+
+
+
+#endif

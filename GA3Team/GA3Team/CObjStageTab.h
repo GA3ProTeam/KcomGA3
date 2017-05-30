8c40f3ff046ac66db0d48e74ca7c981187c48cef
kamiwatariyuki <h1201515011@hiratagakuen.onmicrosoft.com>
2017-05-30 12:15:23 +0900

    ステージセレクトシーン内での各クラスのプロトタイプ完成　【他シーンと共有するクラスはまだ作っていない】

---
+++ GA3Team/GA3Team/CObjStageTab.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef __COBJ_STAGETAB_H__
+#define __COBJ_STAGETAB_H__
+
+//ステージの数(仮)
+#define STAGENUMBER 8
+
+//プレイヤーボタン
+class CObjStageTab : public CObj {
+public:
+	CObjStageTab() {}//コンストラクタ
+	~CObjStageTab() {}//デストラクタ
+
+private:
+	int m_iSelectedCharacterNumber;			  //選んだキャラクター番号
+	ButtonStage* m_sStage_Button[STAGENUMBER];//ステージボタン
+};
+
+#endif//__COBJ_STAGETAB_H__

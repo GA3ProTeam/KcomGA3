86e92e568d352e9d5da8a71405deae13054a857d
藤本一真 <h1201515024@hiratagakuen.onmicrosoft.com>
2017-06-06 11:06:04 +0900

    テスト用ファイル読み込み追加

--- GA3Team/GA3Team/CObjPlayer.cpp
+++ GA3Team/GA3Team/CObjPlayer.cpp
@@ -2,7 +2,16 @@
 
 //イニシャライズ
 void CObjPlayer::Init() {
+	ifstream ifw("1-1.bin", std::ios::in | std::ios::binary);
 
+	ifw.read(reinterpret_cast<char*>(&textlen), sizeof(textlen));
+	ifw.read(reinterpret_cast<char*>(&linecount), sizeof(linecount));
+	ifw.read(reinterpret_cast<char*>(&arrsize), sizeof(arrsize));
+	ifw.read(reinterpret_cast<char*>(&dummy), sizeof(dummy));
+
+	ifw.read(reinterpret_cast<char*>(&tmpstr), textlen * sizeof(char) + 1);
+
+	ifw.close();
 }
 
 //デストラクタ
@@ -19,4 +28,6 @@ void CObjPlayer::Action() {
 void CObjPlayer::Draw() {
 	//カラー情報
 	float col[4] = { 1.0f,1.0f,1.0f,1.0f };
+
+	Font()->StrDraw("test", 0, 0, 16, col);
 }

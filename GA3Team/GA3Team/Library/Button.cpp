2b192db8155006c9f9f215b04e644d6c4f69ca07
東　健太 <h1201515008@hiratagakuen.onmicrosoft.com>
2017-06-06 10:24:01 +0900

    ボタンが押された時の処理を追加

--- GA3Team/GA3Team/Library/Button.cpp
+++ GA3Team/GA3Team/Library/Button.cpp
@@ -1,11 +1,26 @@
 #include "../main.h"
 
-void Button::Push() {
+void Button::Push() //ƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚½Žž
+{
+	int mousex = Input()->m_x;
+	int mousey = Input()->m_y;
 
+	//c‚Æ‰¡(x)
+	if ((mousex > m_iXpos && mousex < (m_iXpos + m_iWidth)) 
+		&& (mousey < m_iYpos && mousey < (m_iYpos + m_iWidth))) 
+	{
+		m_bStatus = true;
+	}
+
+	else
+	{
+		m_bStatus = false;
+	}
 
 }
 
-void Button::Rangedetection() {
+void Button::Rangedetection() 
+{
 
 
 }

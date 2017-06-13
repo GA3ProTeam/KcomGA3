bcf3300aa7529e2b92ec51f06b3eaa960cc40a74
kamiwatariyuki <h1201515011@hiratagakuen.onmicrosoft.com>
2017-06-13 10:01:18 +0900

    ButtonPlayerの中身一通り完成

--- GA3Team/GA3Team/ButtonPlayer.cpp
+++ GA3Team/GA3Team/ButtonPlayer.cpp
@@ -1,11 +1,10 @@
 #include"main.h"
 #include "CSceneStageSelect.h"
-#define CharacterSelect 4
+#define MAXCHARACTERSELECT 4
 
 //イニシャライズ
-void ButtonPlayer::Init() {
-	m_iCharacterNumber = 0;
-	m_bSelected = false;
+void ButtonPlayer::Init(int m_iButtonID) {
+	m_iCharacterNumber = m_iButtonID;
 }
 
 //Destructor
@@ -15,15 +14,17 @@ void ButtonPlayer::Destructor() {
 //action
 void ButtonPlayer::Action() 
 {
+	//ボタンが押されたかどうか確認
 	Push();
-	m_bSelected = true;
+
+	//押されている
+	if (m_bStatus) {
+		//キャラクター番号をステージタブに転送
+		m_sStage_Tab->SetCharacterNum(m_iCharacterNumber);
+	}
 	
-	 
 	
 	
-	//セレクトナンバー　
-	m_iCharacterNumber = 3;
-	m_sStage_Tab->SetCharacterNum(m_iCharacterNumber);
 	
 }
 
@@ -32,22 +33,21 @@ void ButtonPlayer::Draw()
 {
 	float numcolor = 0.5f;//非選択カラー処理
 
-	for (int roopA=0; roopA < CharacterSelect; roopA++) {
+	if (m_bStatus == false) {
 		numcolor = 0.5f;//非選択カラー処理ループ用
-		if (roopA == m_iCharacterNumber) {//もし選択されたキャラまできたら
-			numcolor = 1.0f;//選択カラー処理
-		}
-		
-		//カラー情報
-		float col[4] = { numcolor,numcolor,numcolor,1.0f };
-
-		//切り取り座標
-		m_dst.top = 0; m_dst.left = 0; m_dst.bottom = 1; m_dst.right = 1;
-
-		m_src.top = 0; m_src.left = 0;
-		m_src.bottom = 64 + m_src.top; m_src.right = 64 + m_src.left;
-
-		Image()->Draw(0, &m_src, &m_dst, col, 0.0f);
 	}
+	else{//もし選択されたキャラまできたら
+			numcolor = 1.0f;//選択カラー処理
+	}
+		
+	//カラー情報
+	float col[4] = { numcolor,numcolor,numcolor,1.0f };
 
+	//切り取り座標
+	m_dst.top = 0; m_dst.left = 0; m_dst.bottom = 1; m_dst.right = 1;
+
+	m_src.top = 0; m_src.left = 0;
+	m_src.bottom = 64 + m_src.top; m_src.right = 64 + m_src.left;
+
+	Image()->Draw(0, &m_src, &m_dst, col, 0.0f);
 }

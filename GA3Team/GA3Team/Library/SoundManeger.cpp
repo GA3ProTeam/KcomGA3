0b60bfc38a714c3fdf19cdb20c68ac73adfac95c
butaniyuichi <h12015116001@hiratagakuen.onmicrosoft.com>
2017-06-13 12:13:44 +0900

    修正

--- GA3Team/GA3Team/Library/SoundManeger.cpp
+++ GA3Team/GA3Team/Library/SoundManeger.cpp
@@ -1,4 +1,5 @@
-#include "main.h";
+#include "../main.h"
+
 void CObjSoundManeger::SoundDelete(int soundSlotNum/*削除する音スロットの番号*/)//音を消去する
 {}
 void CObjSoundManeger::SoundSave(int soundNum/*音の番号*/) //音をセーブする

7f3bbbf260d4dde50ba122b5bf3c5dfd82ddb543
東健太 <h1201515008@hiratagakuen.onmicrsoft.com>
2017-06-20 12:11:36 +0900

    セーブデータをセット

--- GA3Team/GA3Team/Library/CObjSavedata.h
+++ GA3Team/GA3Team/Library/CObjSavedata.h
@@ -5,6 +5,11 @@
 class CObjSavedata : public CObj {
 protected:
 	char m_savedata;	  //仮
+	char m_cPlayerName;   //プレイヤーの名前
+	bool m_bSionClearflg; //シオンの進行度
+	bool m_bKouneClearflg;//コウネの進行度
+	bool m_bMelueruClearflg;//メルエルの進行度
+
 public:
 	void Loadsavedata();  // セーブデータ読み込み
 	void Writesavedata(); //セーブデータ書き込み

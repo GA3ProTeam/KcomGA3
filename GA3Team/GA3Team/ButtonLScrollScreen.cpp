e62e37ba7ab321454f9b159bbac4e2b0262c2be4
橋本幸治 <h1201516004@hiratagakuen.onmicrosoft.com>
2017-06-20 10:03:30 +0900

    スクロールボタンクラス修正

--- GA3Team/GA3Team/ButtonLScrollScreen.cpp
+++ GA3Team/GA3Team/ButtonLScrollScreen.cpp
@@ -4,7 +4,7 @@
 void ButtonLScrollScreen::Init() {
 
 
-	m_Button_x = 0;
+	m_Button_x = 64;
 	m_Button_y = 0;
 
 
@@ -21,9 +21,8 @@ void ButtonLScrollScreen::Destructor() {
 //アクション
 void ButtonLScrollScreen::Action() {
 
-	Push();
 
-	if(m_bStatus){
+	if(Push()){
 		User()->mscroll_x-=10;
 	}
 
@@ -31,5 +30,20 @@ void ButtonLScrollScreen::Action() {
 
 //ドロー
 void ButtonLScrollScreen::Draw() {
+
+	//カラー情報
+	float col[4] = { 1.0f,1.0f,1.0f,1.0f };
+
+	//切り取り座標
+	m_dst.top = 33;						m_dst.left = 1;
+	m_dst.bottom = m_dst.top + 64;		m_dst.right = m_dst.left + 32;
+
+	//転送先座標
+	m_src.top = m_Button_y;					m_src.left = m_Button_x;
+	m_src.bottom = m_src.top + 64;		m_src.right = m_src.left + 64;
+
+	//描画
+	Image()->Draw(0, &m_src, &m_dst, col, 0.0f);
+
 
 }
--- GA3Team/GA3Team/ButtonRScrollScreen.cpp
+++ GA3Team/GA3Team/ButtonRScrollScreen.cpp
@@ -21,9 +21,7 @@ void ButtonRScrollScreen::Destructor() {
 //アクション
 void ButtonRScrollScreen::Action() {
 
-	Push();
-
-	if (m_bStatus) {
+	if (Push()) {
 		User()->mscroll_x -= 10;
 	}
 
@@ -31,5 +29,19 @@ void ButtonRScrollScreen::Action() {
 
 //ドロー
 void ButtonRScrollScreen::Draw() {
+
+	//カラー情報
+	float col[4] = { 1.0f,1.0f,1.0f,1.0f };
+
+	//切り取り座標
+	m_dst.top = 33;						m_dst.left = 33;
+	m_dst.bottom = m_dst.top + 64;		m_dst.right = m_dst.left + 64;
+
+	//転送先座標
+	m_src.top = m_Button_y;					m_src.left = m_Button_x;
+	m_src.bottom = m_src.top + 64;		m_src.right = m_src.left + 64;
+
+	//描画
+	Image()->Draw(0, &m_src, &m_dst, col, 0.0f);
 
 }

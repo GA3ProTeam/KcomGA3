6365b812d4e7ad285cea9ccb228ba1757f12b118
藤本一真 <h1201515024@hiratagakuen.onmicrosoft.com>
2017-06-27 11:18:34 +0900

    ギミック修正

--- GA3Team/GA3Team/Library/Gimmick.h
+++ GA3Team/GA3Team/Library/Gimmick.h
@@ -2,14 +2,24 @@
 #define __GIMMICK_H__
 
 class Gimmick : public CObj {
+private:
+	RECT m_src;		//転送先座標
+	RECT m_dst;		//切り取り座標
 protected:
+	//-----------------判定-------------
 	int m_iXpos;	//ギミックの位置(X)
 	int m_iYpos;	//ギミックの位置(Y)
 	int m_iWidth;	//ギミック幅
 	int m_iHeight;	//ギミック高さ
+
+	//----------------表示位置----------
+	int m_iGimXpos;	//ギミックの表示位置(X)
+	int m_iGimYpos; //ギミックの表示位置(Y)
+
 	bool m_bCursor;	//ギミックにカーソルが当たっているかのフラグ
 public:
-	bool gimmickCd();
+	void gimmickDrawSet(int x, int y);
+	bool gimmickCd(); //ギミックの当たり判定
 };
 
 #endif // !__GIMMICK_H__

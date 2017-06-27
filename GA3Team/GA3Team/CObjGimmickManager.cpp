29fb65914ab40c69400bd76169625f80d946e07c
橋本幸治 <h1201516004@hiratagakuen.onmicrosoft.com>
2017-06-27 10:08:01 +0900

    ギミックマネージャー編集

--- GA3Team/GA3Team/CObjGimmickManager.cpp
+++ GA3Team/GA3Team/CObjGimmickManager.cpp
@@ -1,9 +1,41 @@
 #include "main.h"
 
 //イニシャライズ
-void CObjGimmickManager::Init(int stage_id) {
+void CObjGimmickManager::Init(int select_chara, int stage_id) {
 	
-	Stage_ID = stage_id;
+	m_Stage_ID = stage_id;
+
+	switch (m_Stage_ID) {
+	case 1:
+		
+
+
+		break;
+	case 2:
+
+
+
+		break;
+	case 3:
+
+
+
+		break;
+	default:
+		break;
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
 
 
 

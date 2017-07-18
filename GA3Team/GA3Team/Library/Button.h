c8b9986b1f959e83cd687f3dfc632168d1a48a7c
稲葉素晴 <h1201519018@hiratagakuen.onmicrosoft.com>
2017-07-18 17:34:29 +0900

    ボタンクラス編集

--- GA3Team/GA3Team/Library/Button.h
+++ GA3Team/GA3Team/Library/Button.h
@@ -3,13 +3,20 @@
 
 //ボタンクラス（基底）
 class Button : public CObj{
+private:
+	//押したボタンへの参照リスト
+	static list<Button*> m_push_button_list;
+
 protected:
 	bool m_bStatus; //ボタンの状態
 	int m_iXpos;    //ボタンの位置X
 	int m_iYpos;    //ボタンの位置Y
 	int m_iWidth;   //ボタンの幅
 	int m_iHeight;  //ボタンの高さ
+
+	
 public:
+	void Init(int x,int y,int w,int h);//初期化
 	bool Push();//ボタンの当たり判定
 	bool Rangedetection();//ボタンの範囲にカーソルが入っているかどうか
 };

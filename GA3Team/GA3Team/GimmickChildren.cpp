214fcc1e5738f17e0ac5851891862c8c21d13f20
kitanoyuuki <h1201515017@hiratagakuen.onmicrosoft.com>
2017-07-19 09:37:37 +0900

    コメントを追加

--- GA3Team/GA3Team/GimmickChildren.cpp
+++ GA3Team/GA3Team/GimmickChildren.cpp
@@ -1,6 +1,14 @@
 #include "main.h"
 void GimmickChildren::Init(int child_id)///イニシャライズ
 {
+	switch (child_id) {
+	case 1://強気な男の子
+		break;
+	case 2://優しい女の子
+		break;
+	case 3://弱気な男の子
+		break;
+	}
 
 }
 void GimmickChildren::Destructor()//デストラクタ
--- GA3Team/GA3Team/GimmickChildren.h
+++ GA3Team/GA3Team/GimmickChildren.h
@@ -7,7 +7,7 @@ class GimmickChildren : public Gimmick
 private:
 
 public:
-	void Init(int child_id);//イニシャライズ
+	void Init(int child_id);//イニシャライズ(1:強気な男の子　2:優しい女の子　3:弱気な男の子)
 	void Destructor();//デストラクタ
 	void Action();//アクション
 	void Draw();//描画

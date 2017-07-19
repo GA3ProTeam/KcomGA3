29e7d97b5f7255556cf348a7118f8b08a3ea2399
kitanoyuuki <h1201515017@hiratagakuen.onmicrosoft.com>
2017-07-19 09:40:28 +0900

    GimmickTestにGimmickChildrenを追加

--- GA3Team/GA3Team/GimmckTest.h
+++ GA3Team/GA3Team/GimmckTest.h
@@ -12,4 +12,18 @@ private:
 	void Action();				//アクション
 	void Draw();
 };
+
+class GimmickChildren : public Gimmick
+{
+private:
+
+public:
+	void Init(int child_id);//イニシャライズ(1:強気な男の子　2:優しい女の子　3:弱気な男の子)
+	void Destructor();//デストラクタ
+	void Action();//アクション
+	void Draw();//描画
+
+};
+
+
 #endif // !__GIMMCKTEST__

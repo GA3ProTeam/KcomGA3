44ca211e0a7dfa8271fff25af7a5708f61f2db36
藤本一真 <h1201515024@hiratagakuen.onmicrosoft.com>
2017-08-29 14:34:54 +0900

    Coverlay : 修正

--- GA3Team/enc_temp_folder/542864f1172e88a4afa6c76e0f0e0f4/COverlay.h
+++ GA3Team/enc_temp_folder/542864f1172e88a4afa6c76e0f0e0f4/COverlay.h
@@ -8,12 +8,12 @@ enum tutorial
 {
 	//チュートリアル
 	HAKASE_1,
+	HAKASE_CLEAR,
 	HAKASE_FLAG_1_1,
 	HAKASE_FLAG_1_2,
 	HAKASE_FLAG_2_1_NO,
 	HAKASE_FLAG_2_1_YES,
 	HAKASE_FLAG_2_YES_3_NO,
-	HAKASE_CLEAR
 };
 enum sion
 {

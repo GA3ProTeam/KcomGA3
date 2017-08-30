5bcf11c93e616d61d673fea101d8be78d70dcfcc
稲葉素晴 <h1201519018@hiratagakuen.onmicrosoft.com>
2017-08-30 11:20:09 +0900

    GimmickDictor 編集

--- GA3Team/GA3Team/GimmickTest.cpp
+++ GA3Team/GA3Team/GimmickTest.cpp
@@ -214,8 +214,9 @@ void GimmickComputer::Draw() {
 	//---------------------------------------------------------------------------
 
 	//吹き出し描画＆動作---------------------------------------------------------
-
-	this->gimmicDraw(1);
+	if (SavedataManeger()->CurrentData->m_btutoriaruflg[0]) {
+		this->gimmicDraw(1);
+	}
 	//---------------------------------------------------------------------------
 }
 
@@ -240,6 +241,7 @@ void GimmickRecorder::Action() {
 	if (m_ball[0].OnPush) {
 		//フラグ1達成
 		SavedataManeger()->CurrentData->m_btutoriaruflg[0] = true;
+
 		//レコーダー破棄
 		m_Status = STATUS_DELETE;
 
